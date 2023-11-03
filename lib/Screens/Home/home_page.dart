52d838cb5e72daddb3ee11f00d8777e501b77c50
flutterdev-ravan <velmurugan1211r@gmail.com>
2023-11-03 13:17:36 +0530

    Smooth scroll for home

--- lib/Screens/Home/home_page.dart
+++ lib/Screens/Home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:atre_website/Screens/Home/technologyScreen.dart';
 import 'package:atre_website/Utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:web_smooth_scroll/web_smooth_scroll.dart';
 
 import '../navBar.dart';
 import '../BottomSheet.dart';
@@ -15,22 +16,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Controllers
+  ScrollController controller = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Palette.white,
-      body: const Column(
+      body: Column(
         children: [
-          NavBar(),
+          const NavBar(),
           Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  IntroScreen(), //lib/Screens/Home/IntroScreen.dart
-                  TechnologyScreen(), //lib/Screens/Home/technologyScreen.dart
-                  ContactUsScreen(), //lib/Screens/Home/contactUsScreen.dart
-                  AtreBottomSheet() //lib/Screens/BottomSheet.dart
-                ],
+            child: WebSmoothScroll(
+              controller: controller,
+              child: SingleChildScrollView(
+                controller: controller,
+                physics: const NeverScrollableScrollPhysics(),
+                child: const Column(
+                  children: [
+                    IntroScreen(), //lib/Screens/Home/IntroScreen.dart
+                    TechnologyScreen(), //lib/Screens/Home/technologyScreen.dart
+                    ContactUsScreen(), //lib/Screens/Home/contactUsScreen.dart
+                    AtreBottomSheet() //lib/Screens/BottomSheet.dart
+                  ],
+                ),
               ),
             ),
           )
